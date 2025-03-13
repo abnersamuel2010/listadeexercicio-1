19a112280ca1ca1bd55fb629bbc926e944cd6529
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-03-12 23:05:27 -0300

    Create quest6.c

---
+++ quest6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n = 1;
+    int valor = 0;
+    
+    while ( n <= 50) {
+    soma +- n;]
+    n++;
+    }
+    
+    printf("A soma dos numeros de 1 a 50 é: %d\n", soma);
+
+    return 0;
+}
