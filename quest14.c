3ba2d94d6065099035aa1312d0135359e034f585
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-03-17 08:42:50 -0300

    Create quest14.c

---
+++ quest14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int tabuada = 0;
+    int numero, resposta;
+    printf("Escolha um numero pra ver sua tabuada: ");
+    scanf("%d", &numero);
+    while(tabuada < 10){
+        tabuada ++;
+        resposta = numero*tabuada;
+        printf("%d \n",resposta);
+    }
+    return 0;
+}
