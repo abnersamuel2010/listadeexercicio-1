83e3f3c1b989a93ba3c50b17ce76c223692ae263
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-03-17 08:17:10 -0300

    Create quest8.c

---
+++ quest8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n1;
+    int n2;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &n1);
+    
+    printf("Digite outro numero: ");
+    scanf("%d", &n2);
+    
+    if( n1 > n2) {
+        printf("%d", n1);
+    }else if( n1 < n2) { 
+        printf("%d", n2);
+    }
+    return 0;
+}
